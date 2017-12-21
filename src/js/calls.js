7c51a371f5ad9578e90d8e9dd3f345bb63369dc7
DanieleID <dnlpmp@gmail.com>
2017-12-21 15:00:14 +0100

    first complete working version

--- src/js/calls.js
+++ src/js/calls.js
@@ -14,18 +14,24 @@ $.getJSON(URL, function (data) {
     $(".filters-button-group").append('<button class="button" data-filter="' + tags[i] + '">' + tags[i] + '</button>');
   }
   
-$(document).ready(function() {
+  $(document).ready(function() {
   $(".card").on('click', function () {
     $('#proj').css('display','inline-block'); 
     $('body').css('overflow','hidden');
+    var numProj = $(this).attr('id')
+    $('.header').css('background-image', 'url(' + data.project[numProj].cover + ')');
+    $('#nome-proj').html(data.project[numProj].name);
+    $('#descrizione').html(data.project[numProj].descrizione);
+    $('#ggmmyy').html(data.project[numProj].ggmmyy);
   });
-});
+  });
 
   var project = _.map(data.project);
   for (i = 0; i < project.length; i++) {
     var cover = "'" + project[i].cover + "'";
+    var identify = project[i].id;
     var name = project[i].name;
     var tag = project[i].tag;
-    $(".grid").append('<div class="grid-item ' + tag + '"><div class="card" style="background-image: url(' + cover + ')" ><div class="caption d-flex justify-content-center align-items-center flex-column"><h4 class="card-title">' + name + '</h4><ul class="tag d-flex flex-row justify-content-around">' + tag + '</ul></div><div class="card-body hidden-desktop text-center"><h4 class="card-title">' + name + '</h4><ul class="tag d-flex flex-row justify-content-around"><li>' + tag + '</li></ul></div></div></div>');
+    $(".grid").append('<div class="grid-item ' + tag + '"><div class="card" id="' + identify + '"style="background-image: url(' + cover + ')" ><div class="caption d-flex justify-content-center align-items-center flex-column"><h4 class="card-title">' + name + '</h4><ul class="tag d-flex flex-row justify-content-around">' + tag + '</ul></div><div class="card-body hidden-desktop text-center"><h4 class="card-title">' + name + '</h4><ul class="tag d-flex flex-row justify-content-around"><li>' + tag + '</li></ul></div></div></div>');
   }
 });
--- src/js/script.js
+++ src/js/script.js
@@ -2,10 +2,15 @@ $( document ).ready(function() {
   $('.filters-button-group button').on('click', function () {
     $('button').removeClass("is-checked");
     $(this).addClass("is-checked");
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({
-      filter: filterValue
-    });
+    var nome= $(this).text();
+      if (nome != "tutti"){
+        $(".grid-item").hide();
+        $("."+nome).fadeIn("slow");
+      }
+      else {+
+        $(".grid-item").hide();
+        $(".grid-item").fadeIn("slow");
+      }
   });
 
   $('#myModal').on('shown.bs.modal', function () {
