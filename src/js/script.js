7f7df868376e27d50a3e69c311c2366e1042eaa2
DanieleID <dnlpmp@gmail.com>
2017-12-21 12:20:37 +0100

    external function on botton push

--- src/js/script.js
+++ src/js/script.js
@@ -7,6 +7,8 @@ $( document ).ready(function() {
   });
 
   $('.filters-button-group button').on('click', function () {
+    $('button').removeClass("is-checked");
+    $(this).addClass("is-checked");
     var filterValue = $(this).attr('data-filter');
     $grid.isotope({
       filter: filterValue
