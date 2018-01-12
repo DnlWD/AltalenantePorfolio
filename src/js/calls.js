9eb611cfbf39b61ed04b71eab7f47ce562702e4b
DanieleID <dnlpmp@gmail.com>
2018-01-12 15:58:10 +0100

    fix multitag

--- src/js/calls.js
+++ src/js/calls.js
@@ -6,9 +6,7 @@ $.getJSON(URL, function (data) {
   $("#professione").html(data.professione);
   $("#landing").css('background-image', 'url(' + data.landing.url + ')');
   $("#mystyle").attr('href', data.css);
-  var tags = _.uniq(
-    _.map(data.project, "tag")
-  );
+  var tags = _.union.apply(_, _.map(data.project, "tag"));
   $(".filters-button-group").append('<button class="button is-checked" data-filter="*">tutti</button>');
   for (i = 0; i < tags.length; i++) {
     $(".filters-button-group").append('<button class="button" data-filter="' + tags[i] + '">' + tags[i] + '</button>');
@@ -31,7 +29,8 @@ $.getJSON(URL, function (data) {
     var cover = "'" + project[i].cover + "'";
     var identify = project[i].id;
     var name = project[i].name;
-    var tag = project[i].tag;
+    var tags = project[i].tag;
+    var tag = (tags.join(' ')).toString();
     $(".grid").append('<div class="grid-item ' + tag + '"><div class="card" id="' + identify + '"style="background-image: url(' + cover + ')" ><div class="caption d-flex justify-content-center align-items-center flex-column"><h4 class="card-title">' + name + '</h4><ul class="tag d-flex flex-row justify-content-around">' + tag + '</ul></div><div class="card-body hidden-desktop text-center"><h4 class="card-title">' + name + '</h4><ul class="tag d-flex flex-row justify-content-around"><li>' + tag + '</li></ul></div></div></div>');
   }
 });
